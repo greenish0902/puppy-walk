eda136f1f3e2671bd33069d6cbcd2839571ca782
matjji <pon03046@naver.com>
2022-06-19 20:08:07 +0900

    add react/servicecenter

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
 import React from 'react';
 import {Routes, Route} from "react-router-dom"
-import JSCart from './components/JSCart';
-import JSMyPage from './pages/JSMyPage';
-import JSMiniHome from './pages/JSMiniHome';
-import JSMyPurchasePage from './pages/JSMyPurchasePage';
-import JSMyPurchaseList from './pages/JSMyPurchaseList';
-import JSPurchaseDetail from './pages/JSPurchaseDetail';
-import JSMyPost from './pages/JSMyPost';
+
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/mypage" element={<JSMyPage/>}/>
-        <Route path="/minihome" element={<JSMiniHome/>}/>
-        <Route path="/cart" element={<JSCart/>}/>
-        <Route path="/cart/:title/:count/:price" element={<JSCart/>}/>
-        <Route path="/purchase" element={<JSMyPurchasePage/>}/>
-        <Route path="/purchaselist" element={<JSMyPurchaseList/>}/>
-        <Route path="/purchasedetail/:date" element={<JSPurchaseDetail/>}/>
-        <Route path="/post" element={<JSMyPost/>}/>
-      </Routes>
+
     </>
   );
 };
