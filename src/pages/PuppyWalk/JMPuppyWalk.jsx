27130547326c42ed1bf979e120977f735843db71
jeongmo0924 <operbjm@naver.com>
2022-07-21 01:10:57 +0900

    Add - PuppyWalk page

--- src/pages/PuppyWalk/JMPuppyWalk.jsx
+++ src/pages/PuppyWalk/JMPuppyWalk.jsx
@@ -1,14 +1,20 @@
+/**
+ * @filename PuppyWalk.jsx
+ * @description 산책하개 라우팅 페이지
+ * @author 박정모 (oooperbjm@gmail.com)
+ */
+
 import React from 'react';
-import styled from "styled-components";
+import styled from 'styled-components';
 import Header from '../../components/JMHeader';
 import Footer from '../../components/JMFooter';
-import {Routes, Route} from 'react-router-dom';
-import BottomBox from '../../components/Size/BottomBox';
+import { Routes, Route } from 'react-router-dom';
 import PuppyWalkHome from './JMPuppyWalkHome';
-import JMPuppyWalkMate from "./JMPuppyWalkMate";
+import PuppyWalkMate from './JMPuppyWalkMate';
+import MateManage from './JMMateManage';
+import MateConfirm from './JMMateConfirm';
 
 const PuppyWalkContainer = styled.div`
-  background-color: red;
   width: 360px !important;
   height: 600px;
   margin: auto;
@@ -22,16 +28,19 @@ const PuppyWalkContainer = styled.div`
   }
 `;
 const PuppyWalk = () => {
-    return (
-        <PuppyWalkContainer>
-            <Header>같이 산책하개</Header>
-            <Routes>
-                <Route path='/' exact={true} element={<PuppyWalkHome />} />
-                <Route path='/mate' element={<JMPuppyWalkMate />} />
-            </Routes>
-            <Footer />
-        </PuppyWalkContainer>
-    );
+  return (
+    <PuppyWalkContainer>
+      <Header>같이 산책하개</Header>
+      <Routes>
+        <Route path="/" exact={true} element={<PuppyWalkHome />} />
+        <Route path="/mate" element={<PuppyWalkMate />} />
+        <Route path="/matemanage" element={<MateManage />} />
+        <Route path="/mateconfirm" element={<MateConfirm />} />
+
+      </Routes>
+      <Footer />
+    </PuppyWalkContainer>
+  );
 };
 
 export default PuppyWalk;
